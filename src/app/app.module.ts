43df926ea6b660d21ee4d31cd85d86c30fe34f58
Vitovt92 <Vitovt92@rambler.ru>
2020-11-24 23:28:14 +0200

    added material design. First routings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { MaterialModule } from './material.module'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { PosModule } from './pos/pos.module'
+import { UserModule } from './user/user.module'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
