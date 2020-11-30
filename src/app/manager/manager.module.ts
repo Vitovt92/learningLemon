3163a5debab1f83c0e77b6bcce76250fa1eba22e
links <sergeevvs92@gmail.com>
2020-11-30 13:31:53 +0200

    created manager module

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
+import { MaterialModule } from '../material.module'
 import { ManagerHomeComponent } from './manager-home/manager-home.component'
 import { ManagerRoutingModule } from './manager-routing.module'
+import { ManagerComponent } from './manager.component'
 
 @NgModule({
-  declarations: [ManagerHomeComponent],
-  imports: [CommonModule, ManagerRoutingModule],
+  declarations: [ManagerHomeComponent, ManagerComponent],
+  imports: [CommonModule, ManagerRoutingModule, MaterialModule],
 })
 export class ManagerModule {}
